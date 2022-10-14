e05bae1c68708a59cca9b162c13d2d032714f6e3
Koushi Takayama <koushitakayama@KoushinoMacBook-Pro.local>
2022-10-14 22:11:09 +0900

    セットアップファイル呼び出し追加

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
+
+echo "homebrewをインストールします..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "brew doctorを実行します..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor
+
+echo "brew updateを実行します..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update --verbose
+
+echo "brew upgradeを実行します..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew upgrade --verbose
+
+echo ".Brewfileで管理しているアプリケーションをインストールします..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./.Brewfile --verbose
+
+echo "brew cleanupを実行します..."
+which brew >/dev/null 2>&1 && brew cleanup --verbose
 brew bundle
--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+
+setup:.bin/setup.sh
+	cd .bin && source setup.sh
+
 brew:.bin/brew.sh
 	cd .bin && source brew.sh
+
